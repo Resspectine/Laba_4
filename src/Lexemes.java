cf05f569410ea143645635e4f297c04630edd4a7
Resspectine <resspectyn@gmail.com>
2016-10-14 20:27:31 +0300

    Ничего не сделал

--- src/Lexemes.java
+++ src/Lexemes.java
@@ -1,13 +1,9 @@
-/**
- * Created by maks6 on 14.10.16.
- */
-
-import java.util.ArrayList;
+/*import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.Arrays;*/
 public class Lexemes {
     public static void main(String[] args)
     {
-
+        int i=0;
     }
 }
