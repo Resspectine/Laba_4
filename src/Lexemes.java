2ff938b0aeca9088ecc0e55e1c8cb73b217aa26c
Resspectine <resspectyn@gmail.com>
2016-10-14 20:24:03 +0300

    Initial commit

---
+++ src/Lexemes.java
@@ -0,0 +1,13 @@
+/**
+ * Created by maks6 on 14.10.16.
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Lexemes {
+    public static void main(String[] args)
+    {
+
+    }
+}
